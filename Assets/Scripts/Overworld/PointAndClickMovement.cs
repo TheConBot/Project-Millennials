92dbc695cd2e6152ce0741eed625e67487f56265
Connor Botts <connor.botts@gmail.com>
2017-02-27 19:16:25 -0500

    Incontrol + Point and Click + Camera Swivel

---
+++ Assets/Scripts/Overworld/PointAndClickMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PointAndClickMovement : MonoBehaviour {
+
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100000))
+            {
+                agent.destination = hit.point;
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log(Input.GetAxis("Mouse X"));
+            float yAxis = Camera.main.transform.eulerAngles.y; 
+            yAxis += -Input.GetAxis("Mouse X");
+            Camera.main.transform.rotation = Quaternion.Euler(Camera.main.transform.eulerAngles.x, yAxis, Camera.main.transform.eulerAngles.z);
+        }
+    }
+}
