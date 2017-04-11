f52c5ad345fe6767d53c3ae053998bae7e64271d
Connor Botts <connor.botts@gmail.com>
2017-04-11 13:52:39 -0400

    Removed Old Debug Log in PlayerInput

--- Assets/Scripts/Input/PlayerInput.cs
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -66,7 +66,6 @@ public class PlayerInput : MonoBehaviour
         }
         else if (Input.GetMouseButton(1))
         {
-            Debug.Log(Input.GetAxis("Mouse X"));
             float yAxis = cameraPivot.transform.eulerAngles.y * cameraSwivelSpeed;
             yAxis += -Input.GetAxis("Mouse X");
             cameraPivot.transform.rotation = Quaternion.Euler(cameraPivot.transform.eulerAngles.x, yAxis, cameraPivot.transform.eulerAngles.z);
