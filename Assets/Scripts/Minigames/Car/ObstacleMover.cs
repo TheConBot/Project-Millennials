a958db0029cb5bf74fae016aca65f214432d641b
Connor Botts <connor.botts@gmail.com>
2017-05-07 18:50:36 -0400

    Added Car Minigame Set

--- Assets/Scripts/Minigames/Car/ObstacleMover.cs
+++ Assets/Scripts/Minigames/Car/ObstacleMover.cs
@@ -6,13 +6,21 @@ public class ObstacleMover : MonoBehaviour {
 
     public float speed = 5;
     public float zThreshhold = -25;
+    public bool resetOnThreshold;
 
     private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, zThreshhold), Time.deltaTime * speed);
         if(transform.position.z <= zThreshhold)
         {
-            gameObject.SetActive(false);
+            if (!resetOnThreshold)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+            }
         }
     }
 }
---
+++ Assets/Scripts/Utility/DirectionalLightRotator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalLightRotator : MonoBehaviour {
+
+    public float targetRotation;
+    public float rotationMultiplier;
+    public float targetIntensity;
+    public float intensityMultiplier;
+
+    private Light light;
+
+    private void Awake()
+    {
+        light = GetComponent<Light>();
+    }
+
+	void Update () {
+        if (transform.eulerAngles.x > targetRotation)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x - (Time.deltaTime * rotationMultiplier), transform.eulerAngles.y));
+        }
+        if(light.intensity > targetIntensity)
+        {
+            light.intensity -= Time.deltaTime * intensityMultiplier;
+        }
+	}
+}
